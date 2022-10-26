091a37a1cf37e413353f9e0e6cbb1434703e4713
Mark <m7mark@yandex.ru>
2022-10-26 20:38:03 +0300

    fix antd warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './components/screens/Home/Home'
 import './assets/styles/globals.scss'
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<Home />)
