0529bd99fee4bc41766f65cb2434a2934c879815
Mark <m7mark@yandex.ru>
2022-10-28 00:21:32 +0300

    add search button

--- src/components/screens/Workspace/WorkspaceHeader.tsx
+++ src/components/screens/Workspace/WorkspaceHeader.tsx
@@ -1,4 +1,5 @@
 import { Button, Layout } from 'antd'
+import Search from 'antd/lib/input/Search'
 import { FC } from 'react'
 
 import { useDb } from '../../../hooks/useDb'
@@ -36,24 +37,33 @@ export const WorkspaceHeader: FC<IWorkHeader> = ({
   return (
     <Header className={styles.header}>
       <div className={styles.workspaceHeader}>
-        <Button onClick={startEdit} disabled={isEdit || !currentNoteId}>
-          Edit Note
-        </Button>
-        <Button
-          onClick={handleCreateNote}
-          disabled={!isEdit || !currentNoteId}
-          type="primary"
-        >
-          Save
-        </Button>
-        <Button
-          type="primary"
-          onClick={handleDeleteNote}
-          danger
-          disabled={!currentNoteId}
-        >
-          Delete
-        </Button>
+        <div className={styles.search}>
+          <Search
+            placeholder="input search text"
+            onSearch={(value) => console.log(value)}
+            enterButton
+          />
+        </div>
+        <div className={styles.buttons}>
+          <Button onClick={startEdit} disabled={isEdit || !currentNoteId}>
+            Edit Note
+          </Button>
+          <Button
+            onClick={handleCreateNote}
+            disabled={!isEdit || !currentNoteId}
+            type="primary"
+          >
+            Save
+          </Button>
+          <Button
+            type="primary"
+            onClick={handleDeleteNote}
+            danger
+            disabled={!currentNoteId}
+          >
+            Delete
+          </Button>
+        </div>
       </div>
     </Header>
   )
